3192995f300cfbb0dff40bb33025dc1ad7cf116e
Paul John <rainson.work@gmail.com>
2023-01-24 16:55:02 +0300

    test file

---
+++ test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - testing collaboration
+ *
+ * Retun: 0 (success)
+ */
+int main(void)
+{
+	puts("If you can see this");
+	puts("It means I found the way out!");
+
+	return (0);
+}
